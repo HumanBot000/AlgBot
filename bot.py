d4c52d83127030d4e80a59e308ee153a73d00284
tobia <tobias.sarosi@gmail.com>
2024-02-02 22:27:41 +0100

    implemented checkmate check

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ import chess
 import chess.svg
 
 game = chess.Board()
-
-
+INFINITY = 100000
+SHOW_SVG = True
 def handle_user():
     user_move = input(">You:")
     try:
@@ -19,25 +19,36 @@ def handle_user():
         print("Invalid Move!")
         handle_user()
 
+
 def get_difference(color=chess.BLACK):
     if color == chess.BLACK:
         return get_current_score(chess.BLACK) - get_current_score(chess.WHITE)
     return get_current_score(chess.WHITE) - get_current_score(chess.BLACK)
+
+
+
 def get_current_score(color=chess.BLACK):
     score = 0
     score += len(game.pieces(chess.PAWN, color)) * 1
     score += len(game.pieces(chess.BISHOP, color)) * 3
     score += len(game.pieces(chess.KNIGHT, color)) * 3
-    score += len(game.pieces(chess.ROOK,color)) * 5
+    score += len(game.pieces(chess.ROOK, color)) * 5
     score += len(game.pieces(chess.QUEEN, color)) * 9
+    if game.outcome() != None and game.outcome().winner == color:
+        score += INFINITY
+    elif game.outcome() != None and game.outcome().winner != color:
+        score -= INFINITY
     return score
+
 
 def show_board_svg():
     boardsvg = chess.svg.board(game, size=600, coordinates=True)
     with open('temp.svg', 'w') as outputfile:
         outputfile.write(boardsvg)
     time.sleep(0.1)
-    os.startfile('temp.svg')
+    if SHOW_SVG:os.startfile('temp.svg')
+
+
 def handle_bot():
     my_best_move_rating = -1000
     my_best_move = None
@@ -54,12 +65,14 @@ def handle_bot():
         game.push(opponent_best_move)
         if get_difference(chess.BLACK) > my_best_move_rating:
             my_best_move_rating = get_difference(chess.BLACK)
-            my_best_move= my_move
+            my_best_move = my_move
         game.pop()
         game.pop()
     game.push(my_best_move)
     print(f">Bot: {my_best_move}")
     return my_best_move
+
+
 while not game.is_game_over():
     show_board_svg()
     print(game)
