e9f0e55900db179a591309d4477fb7f186ade6fe
tobia <tobias.sarosi@gmail.com>
2024-02-02 22:19:14 +0100

    Initial commit

---
+++ bot.py
@@ -0,0 +1,70 @@
+import os
+import random
+import time
+
+import chess
+import chess.svg
+
+game = chess.Board()
+
+
+def handle_user():
+    user_move = input(">You:")
+    try:
+        game.push_san(user_move)
+    except chess.IllegalMoveError:
+        print("Illegal Move!")
+        handle_user()
+    except chess.InvalidMoveError:
+        print("Invalid Move!")
+        handle_user()
+
+def get_difference(color=chess.BLACK):
+    if color == chess.BLACK:
+        return get_current_score(chess.BLACK) - get_current_score(chess.WHITE)
+    return get_current_score(chess.WHITE) - get_current_score(chess.BLACK)
+def get_current_score(color=chess.BLACK):
+    score = 0
+    score += len(game.pieces(chess.PAWN, color)) * 1
+    score += len(game.pieces(chess.BISHOP, color)) * 3
+    score += len(game.pieces(chess.KNIGHT, color)) * 3
+    score += len(game.pieces(chess.ROOK,color)) * 5
+    score += len(game.pieces(chess.QUEEN, color)) * 9
+    return score
+
+def show_board_svg():
+    boardsvg = chess.svg.board(game, size=600, coordinates=True)
+    with open('temp.svg', 'w') as outputfile:
+        outputfile.write(boardsvg)
+    time.sleep(0.1)
+    os.startfile('temp.svg')
+def handle_bot():
+    my_best_move_rating = -1000
+    my_best_move = None
+    for my_move in game.legal_moves:
+        game.push(my_move)
+        opponent_best_move_rating = -1000
+        opponent_best_move = None
+        for opponent_move in game.legal_moves:
+            game.push(opponent_move)
+            if get_difference(chess.WHITE) > opponent_best_move_rating:
+                opponent_best_move_rating = get_difference(chess.WHITE)
+                opponent_best_move = opponent_move
+            game.pop()
+        game.push(opponent_best_move)
+        if get_difference(chess.BLACK) > my_best_move_rating:
+            my_best_move_rating = get_difference(chess.BLACK)
+            my_best_move= my_move
+        game.pop()
+        game.pop()
+    game.push(my_best_move)
+    print(f">Bot: {my_best_move}")
+    return my_best_move
+while not game.is_game_over():
+    show_board_svg()
+    print(game)
+    handle_user()
+    if game.is_game_over():
+        break
+    show_board_svg()
+    handle_bot()
