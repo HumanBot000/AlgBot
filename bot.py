03d9b2c31c992d39826963bc6c2c238b130a525d
HumanBot000 <tobias.sarosi@gmail.com>
2024-02-07 10:14:41 +0100

    Implemented passing pawn check

--- bot.py
+++ bot.py
@@ -9,7 +9,8 @@ import chess.svg
 game = chess.Board()
 INFINITY = 100000
 SHOW_SVG = True
-BOT_PLAYS_AS = chess.BLACK
+BOT_PLAYS_AS = chess.WHITE
+
 
 def handle_user():
     user_move = input(">You:")
@@ -28,35 +29,67 @@ def get_difference(color=chess.BLACK):
         return get_current_score(chess.BLACK) - get_current_score(chess.WHITE)
     return get_current_score(chess.WHITE) - get_current_score(chess.BLACK)
 
+
 def position_score(score, color):
     with open('filePositionAdvantages.json', 'r') as f:
         json_file_files = json.loads(f.read())
     with open('rankPositionAdvantages.json', 'r') as f:
         json_file_ranks = json.loads(f.read())
     for square in game.pieces(chess.KNIGHT, color):
-        number = chess.square_rank(square)+1
-        letter = chess.square_file(square)+1
+        number = chess.square_rank(square) + 1
+        letter = chess.square_file(square) + 1
         if color == chess.WHITE:
             score += int(json_file_files["KnightWhite"][str(number)])
         else:
             score += int(json_file_files["KnightBlack"][str(number)])
         score += int(json_file_ranks["Knight"][str(letter)])
     for square in game.pieces(chess.PAWN, color):
-        number = chess.square_file(square)+1
-        letter = chess.square_rank(square)+1
+        number = chess.square_rank(square) + 1
+        letter = chess.square_file(square) + 1
         if color == chess.WHITE:
             score += int(json_file_files["PawnWhite"][str(number)])
         else:
             score += int(json_file_files["PawnBlack"][str(number)])
     for square in game.pieces(chess.BISHOP, color):
-        number = chess.square_file(square)+1
-        letter = chess.square_rank(square)+1
+        number = chess.square_rank(square) + 1
+        letter = chess.square_file(square) + 1
         if color == chess.WHITE:
             score += int(json_file_files["BishopWhite"][str(number)])
         else:
             score += int(json_file_files["BishopBlack"][str(number)])
         score += int(json_file_ranks["Bishop"][str(letter)])
     return score
+
+
+def check_passing_pawn(score,color=chess.BLACK):
+    if len(game.pieces(chess.PAWN, color)) == 0:
+        return score
+    for my_piece_position in game.pieces(chess.PAWN, color):
+        file = chess.square_file(my_piece_position)
+        rank = chess.square_rank(my_piece_position)
+        if not file == 0 or file == 7:
+            files = [file-1,file,file+1]
+        elif file == 0:
+            files = [file,file+1]
+        elif file == 7:
+            files = [file-1,file]
+        for scanning_file in files:
+            if color == chess.WHITE:
+                for scanning_rank in range(rank,8):
+                    if game.piece_at(chess.square(scanning_file,scanning_rank)) is not None and game.piece_at(chess.square(scanning_file,scanning_rank)).piece_type == chess.PAWN and game.piece_at(chess.square(scanning_file,scanning_rank)).color == get_opposite_color(color):
+                        return score
+            else:
+                for scanning_rank in range(rank,0,-1):
+                    if game.piece_at(chess.square(scanning_file,scanning_rank)) is not None and game.piece_at(chess.square(scanning_file,scanning_rank)).piece_type == chess.PAWN and game.piece_at(chess.square(scanning_file,scanning_rank)).color == get_opposite_color(color):
+                        return score
+    print("PASSING PAWN")
+    return score+2.4
+
+def get_opposite_color(color):
+    if color == chess.WHITE:
+        return chess.BLACK
+    else:
+        return chess.WHITE
 
 def get_current_score(color=chess.BLACK):
     score = 0
@@ -67,9 +100,10 @@ def get_current_score(color=chess.BLACK):
     score += len(game.pieces(chess.QUEEN, color)) * 9
     if game.outcome() != None and game.outcome().winner == color:
         score += INFINITY
-    elif game.outcome() != None and game.outcome().winner != color:
+    elif game.outcome() != None and game.outcome().winner == get_opposite_color(color):
         score -= INFINITY
     score = position_score(score, color)
+    score = check_passing_pawn(score,color)
     return score
 
 
@@ -124,5 +158,3 @@ while not game.is_game_over():
         if game.is_game_over():
             break
         handle_user()
-
-
